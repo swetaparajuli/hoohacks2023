0e926a4c21fe9d67451e4b0d4eb414c10550ade2
swetaparajuli <parajulis4@vcu.edu>
2023-03-25 16:53:57 -0400

    Users can now make posts and they will display

--- app.py
+++ app.py
@@ -1,9 +1,30 @@
+from datetime import datetime
+
 from flask import Flask, render_template, redirect, url_for
 from flask_bootstrap import Bootstrap
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField, SelectField, EmailField, IntegerField, TextAreaField
+from wtforms.validators import DataRequired
+from models import db, Post
 
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'very secretive and difficult to figure out key'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
+db.init_app(app)
 bootstrap = Bootstrap(app)
+
+# Create the tables in the database
+with app.app_context():
+    db.create_all()
+
+
+class MessageForm(FlaskForm):
+    title = StringField('Title:', validators=[DataRequired()])
+    message = TextAreaField("Share your story: ", validators=[DataRequired()])
+
+    submit = SubmitField('Post')
+
 
 @app.errorhandler(404)
 def page_not_found(e):
@@ -15,8 +36,27 @@ def internal_server_error(e):
     return render_template('500.html'), 500
 
 
+@app.route('/posts')
+def submitted():
+    posts = Post.query.order_by(Post.date_created.desc()).all()
+    return render_template('posts.html', posts=posts)
+
+
+@app.route('/create', methods=['GET', 'POST'])
+def create_post():
+    form = MessageForm()
+    if form.validate_on_submit():
+        title = form.title.data
+        content = form.message.data
+        post = Post(title=title, content=content, date_created=datetime.utcnow())
+        db.session.add(post)
+        db.session.commit()
+        return redirect(url_for('submitted'))
+    return render_template('create.html', form=form)
+
+
 @app.route('/')
-def hello_world():  # put application's code here
+def index():  # put application's code here
     return render_template('index.html')
 
 
---
+++ models.py
@@ -0,0 +1,12 @@
+from datetime import datetime
+
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
+
+
+class Post(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(100))
+    content = db.Column(db.Text)
+    date_created = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
